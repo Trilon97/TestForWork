a467a04e556bf1d8fe4c42c9c0c9fc38440216b9
Trilon97 <110154778+Trilon97@users.noreply.github.com>
2022-08-05 02:20:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,12 +8,11 @@ function $ (id) //return ElementById but shorter
   return document.getElementById(id);
 }
 
-function SecretsVisibility(elem)
+function SecretsVisibility(elem)//swich between the visibility of the sections
 {
-  console.log(elem.target.name)
   if (getClassById("."+elem.target.name)[0].style.display == "inline")
   {
-    getClassById("."+elem.target.name).forEach(function(el) {
+    getClassById("."+elem.target.name).forEach(function(el) { 
        el.style.display = 'none';
     });
   } else
@@ -51,14 +50,3 @@ function clearAddParameters()
     $("addSecretExpireDate").value = ""
 }
 $("clearAddSecretsParameters").addEventListener("click",clearAddParameters)
-
-/*elseif($check == 1)
-        {
-          echo "<a class='warning'>This code is not exist!</a><br>";
-        }elseif($check == 2)
-        {
-          echo "<a class='warning'>Reading limit is out!</a><br>";
-        }elseif($check == 3)
-        {
-          echo "<a class='warning'>The secrets time has expired!</a><br>";
-        }*/
